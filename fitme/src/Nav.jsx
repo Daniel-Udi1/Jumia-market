671e8ebea38b9670da1a808a730a68011a4a3884
Danieludi97 <Danieluweudi@gmail.com>
2026-08-26 12:47:08 +0100

    fitme

---
+++ fitme/src/Nav.jsx
@@ -0,0 +1,85 @@
+import {react} from "react"
+import bag from "./assets/image/Bag.png"
+import subtract from "./assets/image/Subtract.png"
+import { FaSearch } from "react-icons/fa";
+
+function Nav () {
+
+return(
+
+<>
+
+    <div className=" flex justify-between px-10 pt-3">
+          
+          <div className="flex items-center gap-1.5">
+            <img src={subtract} className="w-6" />
+            <p className="font-bold text-black text-2xl">Fitme</p>
+          </div>
+        
+         <div className="flex justify-between gap-7">
+
+            <div className="flex items-center  gap-2 border-2 rounded">
+                 <input className=" ml-4  w-96 " placeholder="Enter item or restaurant you are looking for"></input>
+             <  FaSearch className="mr-4" />
+            </div>
+
+             
+
+             <div className="flex justify-between px-3 gap-4 items-center">
+                <img src={bag} className=""/>
+             <button className="bg-black text-white h-8 w-20 rounded ">sign in</button>
+             </div>
+            
+       
+         </div>
+    </div>
+
+
+
+
+</>
+
+)
+
+
+
+
+}
+
+export default Nav;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ fitme/src/Section.jsx
@@ -0,0 +1,122 @@
+import {react} from "react"
+import union from "/src/assets/image/Union.png"
+import component from "/src/assets/image/Component 4.png"
+import compo from "/src/assets/image/Component 5.png"
+import Reatangle from "/src/assets/image/Rectangle 1.png"
+import Red from "/src/assets/image/Rectangle 2.png"
+
+
+function Section(){
+
+
+return(
+
+<>
+
+ 
+<div className="flex justify-center pt-32 ">
+
+ 
+<div>
+     <img src={union} className="flex justify-between items-center pt-20 mr-9" />
+     
+ </div>
+
+
+
+      <div >
+
+        
+        <div className="flex gap-2 pt-3">
+          <p className="text-black font-bold text-2xl">Premium </p>
+          <p className="text-yellow-300 font-bold text-2xl">Quality</p>
+         </div>
+
+
+         <div className="flex pt-6 gap-3" >
+            <p className="font-sans text-black text-3xl">Food for your</p>
+            <img src={component} className="w-15 "/>
+            <p className="font-sans text-yellow-400 text-3xl">healthy</p>
+
+         </div>
+
+      
+                    <div className="flex pt-7">
+                        <img src={compo}  className="w-15"/>
+                        <p className="font-sans text-yellow-500 text-3xl ml-4">& Daily Life</p>
+                    </div>
+
+            
+            <div className="pt-5">
+              <p cl>Lorem ipsum dolor sit amet, consectetur incididunt  ut labore </p>
+                 <p className="pt-2">Lorem ipsum dolor sit amet, consectetur incididunt  ut labore </p>
+                  <p className="pt-2">Lorem ipsum dolor sit amet, consectetur incididunt  ut labore </p>
+                   <p className="pt-2">Lorem ipsum dolor sit amet, consectetur incididunt  ut labore </p>
+
+
+
+            </div>
+
+              
+            <div className="flex pt-4  gap-6">
+              
+             <input className=" w-64 border-2 rounded  px-4" placeholder="Enter your delivery location" />
+             <button className="bg-black rounded text-white w-32">Get Started</button>
+
+              </div>            
+               
+
+               <div className="pt-6">
+                <p>Popular cities in India</p>
+               </div>
+
+               <div className="flex gap-3 pt-4">
+                 
+                 <p>Hyderabad</p>
+                 <p  className="text-amber-600">Chennal</p>
+                 <p>Mumbai</p>
+                 <p  className="text-amber-600">Bangalore</p>
+                 <p >Delhi</p>
+                 <p className="text-amber-600">Kolkata</p>
+
+
+
+               </div>
+
+
+      </div>
+
+
+
+         <div className="flex gap-3.5 ml-6">
+              <img src={Reatangle} className="h-[500px]" />
+              <img src={Red} className="h-[450px] pt-20" />
+
+             </div>
+
+
+
+
+
+
+
+
+</div>
+
+
+
+
+
+
+
+</>
+
+
+
+)
+
+
+
+}
+
+export default Section;
